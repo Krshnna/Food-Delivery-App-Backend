0bae539bb77d66e977d5c74eeb9eea9a7146be8d
Krishna Kapoor <88818689+Krshnna@users.noreply.github.com>
2024-02-15 21:13:30 +0530

    Added user model with pre save password

---
+++ src/models/user.models.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (v) {
+          return /\b[A-Za-z0-9._%+-]+@gmail\.com\b/.test(v);
+        },
+        message: (props) => `${props.value} is not a valid email!`,
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    avatar: {
+      public_id: {
+        type: String,
+        required: [true, "Public Id is required."],
+      },
+      url: {
+        type: String,
+        required: [true, "Url is required."],
+      },
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+
+userSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  }
+  next();
+});
+
+export const User = mongoose.model("User", userSchema);
